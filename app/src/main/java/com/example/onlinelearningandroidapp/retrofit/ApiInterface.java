1b44a9196610850401c7a31bba5f2cf2294214ab
andrei <git1@email.com>
2021-10-11 17:40:53 +0200

    added API GET method

--- app/src/main/java/com/example/onlinelearningandroidapp/retrofit/ApiInterface.java
+++ app/src/main/java/com/example/onlinelearningandroidapp/retrofit/ApiInterface.java
@@ -1,9 +1,15 @@
 package com.example.onlinelearningandroidapp.retrofit;
 
+import com.example.onlinelearningandroidapp.model.Category;
+
+import java.util.List;
+
+import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ApiInterface {
 
-    @GET("courses.json")
+    @GET("courses")
+    Call<List<Category>> getAllCategory();
 
 }
