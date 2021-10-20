4b82b98381446746720d4c2e5eb3c27442b5ed25
andrei <git1@email.com>
2021-10-20 17:31:35 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/onlinelearningandroidapp/MainActivity.java
+++ app/src/main/java/com/example/onlinelearningandroidapp/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.onlinelearningandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.onlinelearningandroidapp.model.Category;
+import com.example.onlinelearningandroidapp.retrofit.ApiInterface;
+import com.example.onlinelearningandroidapp.retrofit.RetrofitClient;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {f
+
+    ApiInterface apiInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        apiInterface = RetrofitClient.getRetrofitClient().create(ApiInterface.class);
+
+        Call<List<Category>> call = apiInterface.getAllCategory();
+
+        call.enqueue(new Callback<List<Category>>() {
+            @Override
+            public void onResponse(Call<List<Category>> call, Response<List<Category>> response) {
+                List<Category> categoryList = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<List<Category>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "No response from server", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
